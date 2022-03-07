848a0a307ce37fb8047bf6c75e010b38c7884035
Alex Marshall <alex.k.marshall83@gmail.com>
2022-03-07 14:33:56 +0000

    chore: initialize project

---
+++ app/root.tsx
@@ -0,0 +1,24 @@
+import { Links, LiveReload, Meta, Outlet, ScrollRestoration } from 'remix'
+import type { MetaFunction } from 'remix'
+
+export const meta: MetaFunction = () => {
+  return { title: 'Entertainment App' }
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <LiveReload />
+      </body>
+    </html>
+  )
+}
