3c70a502002f7044c1b554ec8e98c51cdbec09ba
Alex Marshall <alex.k.marshall83@gmail.com>
2022-03-21 07:22:47 +0000

    style: add padding to reel scroll-bar

--- app/components/media-reel/media-reel.tsx
+++ app/components/media-reel/media-reel.tsx
@@ -1,10 +1,37 @@
+import { ReactNode, useEffect, useRef } from 'react'
+
 import { LinksFunction } from 'remix'
-import { ReactNode } from 'react'
 import styles from './media-reel.css'
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
 
 type Props = { children: ReactNode }
 export function MediaReel({ children }: Props): JSX.Element {
-  return <div className="media-reel">{children}</div>
+  const reelRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const toggleOverflowClass = (el: Element) => {
+      el.classList.toggle('overflowing', el.scrollWidth > el.clientWidth)
+    }
+
+    const reel = reelRef.current
+    if (reel) {
+      const observer = new ResizeObserver((entries) => {
+        const [{ target }] = entries
+        toggleOverflowClass(target)
+      })
+
+      observer.observe(reel)
+
+      return () => {
+        observer.disconnect()
+      }
+    }
+  }, [])
+
+  return (
+    <div className="media-reel" ref={reelRef}>
+      {children}
+    </div>
+  )
 }
