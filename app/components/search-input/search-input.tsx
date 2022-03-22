f15afd468fdc7570836d789704d39c2a5da94f8d
Alex Marshall <alex.k.marshall83@gmail.com>
2022-03-22 01:47:22 +0000

    feat: create search input component

---
+++ app/components/search-input/search-input.tsx
@@ -0,0 +1,28 @@
+import { LinksFunction } from 'remix'
+import { SearchIcon } from '~/components/icons'
+import styles from './search-input.css'
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
+
+type Props = {
+  label: string
+  inputProps: { id: string }
+}
+export function SearchInput({ inputProps, label }: Props): JSX.Element {
+  return (
+    <div className="search-input">
+      <SearchIcon />
+      <label htmlFor={inputProps.id} className="visually-hidden">
+        {label}
+      </label>
+      <input
+        type="search"
+        placeholder={label}
+        id={inputProps.id}
+        autoComplete="off"
+        autoCorrect="off"
+        autoCapitalize="off"
+      />
+    </div>
+  )
+}
