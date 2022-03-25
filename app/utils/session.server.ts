ad28dabe15f8d150dec68220cdcef5e5432003de
Alex Marshall <alex.k.marshall83@gmail.com>
2022-03-25 18:38:55 +0000

    feat: create createUserSession util

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,3 +1,5 @@
+import { createCookieSessionStorage, redirect } from 'remix'
+
 import bcrypt from 'bcryptjs'
 import { db } from './db.server'
 
@@ -18,4 +20,31 @@ export async function login({ email, password }: LoginForm) {
   if (!isCorrectPassword) return null
 
   return { id: user.id, email }
+}
+
+const sessionSecret = process.env.SESSION_SECRET
+if (!sessionSecret) {
+  throw new Error('SESSION_SECRET must be set')
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: 'EWA_session',
+    secure: true,
+    secrets: [sessionSecret],
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+})
+
+export async function createUserSession(userId: string, redirectTo: string) {
+  const session = await storage.getSession()
+  session.set('userId', userId)
+  return redirect(redirectTo, {
+    headers: {
+      'Set-Cookie': await storage.commitSession(session),
+    },
+  })
 }
