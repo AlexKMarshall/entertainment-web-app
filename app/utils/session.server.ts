4973f79646478a3f83075f43b59a2d3403612497
Alex Marshall <alex.k.marshall83@gmail.com>
2022-03-25 20:36:22 +0000

    feat: create logout function

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -63,6 +63,32 @@ export async function requireUserId(
   return userId
 }
 
+export async function getUser(request: Request) {
+  const userId = await getUserId(request)
+  if (typeof userId !== 'string') {
+    return null
+  }
+
+  try {
+    const user = await db.user.findUnique({
+      where: { id: userId },
+      select: { id: true, email: true },
+    })
+    return user
+  } catch {
+    throw logout(request)
+  }
+}
+
+export async function logout(request: Request) {
+  const session = await getUserSession(request)
+  return redirect('/login', {
+    headers: {
+      'Set-Cookie': await storage.destroySession(session),
+    },
+  })
+}
+
 export async function createUserSession(userId: string, redirectTo: string) {
   const session = await storage.getSession()
   session.set('userId', userId)
