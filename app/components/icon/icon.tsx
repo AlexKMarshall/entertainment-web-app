69a791807623c7e69b423bb2f6f2f4cd56e95cc7
Alex Marshall <alex.k.marshall83@gmail.com>
2022-03-25 11:18:23 +0000

    feat: create BookmarkOutline icon

--- app/components/icon/icon.tsx
+++ app/components/icon/icon.tsx
@@ -1,12 +1,23 @@
 import { ReactNode } from 'react'
 
-type IconBaseProps = { path: ReactNode; className?: string; viewBox: string }
-function IconBase({ path, className, viewBox }: IconBaseProps): JSX.Element {
+type IconBaseProps = {
+  path: ReactNode
+  className?: string
+  viewBox: string
+  type: 'fill' | 'stroke'
+}
+function IconBase({
+  path,
+  className,
+  viewBox,
+  type = 'fill',
+}: IconBaseProps): JSX.Element {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox={viewBox}
-      fill="currentColor"
+      fill={type === 'fill' ? 'currentColor' : 'none'}
+      stroke={type === 'stroke' ? 'currentColor' : 'none'}
       width="1em"
       className={className}
     >
@@ -20,10 +31,11 @@ type IconProps = Pick<IconBaseProps, 'className'>
 export const createIcon = (
   path: ReactNode,
   displayName: string,
-  viewBox: string
+  viewBox: string,
+  type: IconBaseProps['type'] = 'fill'
 ) => {
   const Icon = ({ className }: IconProps) => (
-    <IconBase path={path} className={className} viewBox={viewBox} />
+    <IconBase path={path} className={className} viewBox={viewBox} type={type} />
   )
   Icon.displayName = displayName
   return Icon
