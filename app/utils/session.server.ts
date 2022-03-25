6ff561f54a1b0dfd7a255975c2dea6ce2b5879c7
Alex Marshall <alex.k.marshall83@gmail.com>
2022-03-25 18:50:29 +0000

    feat: create requireUserId util

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -39,6 +39,30 @@ const storage = createCookieSessionStorage({
   },
 })
 
+function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get('Cookie'))
+}
+
+export async function getUserId(request: Request) {
+  const session = await getUserSession(request)
+  const userId = session.get('userId')
+  if (!userId || typeof userId !== 'string') return null
+  return userId
+}
+
+export async function requireUserId(
+  request: Request,
+  redirectTo: string = new URL(request.url).pathname
+) {
+  const session = await getUserSession(request)
+  const userId = session.get('userId')
+  if (!userId || typeof userId !== 'string') {
+    const searchParams = new URLSearchParams([['redirectTo', redirectTo]])
+    throw redirect(`/login?${searchParams}`)
+  }
+  return userId
+}
+
 export async function createUserSession(userId: string, redirectTo: string) {
   const session = await storage.getSession()
   session.set('userId', userId)
