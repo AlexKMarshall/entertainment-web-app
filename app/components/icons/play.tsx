440579e0f12b96073299b3b5011764c58b99268f
Alex Marshall <alex.k.marshall83@gmail.com>
2022-03-26 10:10:08 +0000

    feat: create play icon

---
+++ app/components/icons/play.tsx
@@ -0,0 +1,7 @@
+import { createIcon } from '~/components/icon'
+
+export const PlayIcon = createIcon(
+  <path d="M15 0C6.713 0 0 6.713 0 15c0 8.288 6.713 15 15 15 8.288 0 15-6.712 15-15 0-8.287-6.712-15-15-15Zm-3 21V8l9 6.5-9 6.5Z" />,
+  'PlayIcon',
+  '0 0 30 30'
+)
